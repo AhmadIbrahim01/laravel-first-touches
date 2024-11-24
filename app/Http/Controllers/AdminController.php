1cc5e604e4c9ed744b7911cda2f76f89f32a47b5
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-11-24 02:42:40 +0200

    Add: add_admin Method Inside AdminController Class

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Admin;
+
 
 class AdminController extends Controller
 {
-    //
+    function add_admin(Request $request){
+        $admin = Admin::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => $request->password,
+        ]);
+
+        return response()->json([
+            "New_admin" => $admin,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\AdminController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::post("/add",[AdminController::class, "add_admin"]);
